061dda2b8c0584496a95979d3a9936e13beb084d
Olbog <olbog2009@bk.ru>
2022-03-22 17:30:25 +0300

    aded polymorphism about interfaces

--- src/homeWork/Runner.java
+++ src/homeWork/Runner.java
@@ -19,12 +19,21 @@ public class Runner {
         System.out.println(moto.hashCode());
         System.out.println(moto2.hashCode());
         System.out.println(moto.equals(moto2));
+
+        IMoveGoods truck = new Truck("blue", "6 m.",18, 4 );
+        IMoveGoods sedan2 = new Sedan("Green","2.3 m.", 4, 5 );
+        start(sedan2);
     }
+
     public static void start (Vehicles vehicles){
         Stantions departureStantion = new Stantions("Minsk");
         Stantions destinationStation = new Stantions("Brest");
         Passengers passengers = new Passengers(10, 1);
         Driver driver = new Driver("woman", "Kate", 29);
         vehicles.move(departureStantion, destinationStation, passengers, driver);
+    }
+    public static void start (IMoveGoods IMoveGoods){
+        IMoveGoods.moveGoods();
+
     }
 }
